8a7e46ff1b753d2ed19bf9d72b75d58612a9f2f8
John Hurst <john.b.hurst@gmail.com>
2010-07-02 15:55:13 +1200

    Added build.gradle and beginnings of project.

---
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/PDFBuilder.groovy
@@ -0,0 +1,5 @@
+package nz.co.skepticalhumorist.pdfbuilder
+
+class PDFBuilder {
+
+}
---
+++ src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/PDFBuilderTest.groovy
@@ -0,0 +1,12 @@
+package nz.co.skepticalhumorist.pdfbuilder
+
+import org.junit.Test
+
+class PDFBuilderTest {
+  @Test
+  void testOk() {
+    def builder = new PDFBuilder()
+  }
+
+}
+

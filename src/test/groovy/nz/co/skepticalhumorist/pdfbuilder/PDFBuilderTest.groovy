b9be27f3dbfab104245a42ed5dea110e3d555563
John Hurst <john.b.hurst@gmail.com>
2010-07-09 19:06:58 +1200

    Restructured test case, shortened.

--- src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/PDFBuilderTest.groovy
+++ src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/PDFBuilderTest.groovy
@@ -1,26 +1,21 @@
 package nz.co.skepticalhumorist.pdfbuilder
 
 import org.junit.Test
-import java.awt.Color
-import com.itextpdf.text.Font
 import com.itextpdf.text.Document
 
 class PDFBuilderTest {
   @Test
   void testOk() {
-    new File("tryme.pdf").withOutputStream {outputStream ->
-      def document = new PDFBuilder(outputStream: outputStream).document() {
-        image(filename: "images/LowagieBook.png")
-        paragraph(string: "First paragraph")
-        paragraph("Second paragraph")
-        table(numColumns: 2) {
-          cell(string: "cell 1,1"); cell(string: "cell 1,2")
-          cell("cell 2,1"); cell("cell 2,2")
-        } 
-//        paragraph(string: "Invoice", font: new Font(Font.HELVETICA, 16, Font.BOLD, Color.BLACK))
+    def document = new PDFBuilder(outputStream: new File("tryme.pdf").newOutputStream()).document() {
+      image(filename: "images/LowagieBook.png")
+      paragraph(string: "First paragraph")
+      paragraph("Second paragraph")
+      table(numColumns: 2) {
+        cell(string: "cell 1,1"); cell(string: "cell 1,2")
+        cell("cell 2,1"); cell("cell 2,2")
       }
-      assert document.class == Document
     }
+    assert document.class == Document
   }
 
 }

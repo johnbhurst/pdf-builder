7a320dc8012923d78fffcf87b8d3ddf6ef681fa9
John Hurst <john.b.hurst@gmail.com>
2010-07-09 20:26:51 +1200

    Simplified handling of constructor arguments in Paragraph.

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ParagraphFactory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ParagraphFactory.groovy
@@ -2,16 +2,57 @@ package nz.co.skepticalhumorist.pdfbuilder
 
 import com.itextpdf.text.Font
 import com.itextpdf.text.Paragraph
+import com.itextpdf.text.Chunk
+import com.itextpdf.text.Phrase
 
 class ParagraphFactory extends AbstractFactory {
 
   Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
-    String string = attributes.containsKey("string") ? attributes.remove("string") : value
-    if (attributes.containsKey("font")) {
+    Paragraph result = null
+    if (attributes.containsKey("leading") && attributes.containsKey("string") && attributes.containsKey("font")) {
+      float leading = attributes.remove("leading")
+      String string = attributes.remove("string")
       Font font = attributes.remove("font")
-      return new Paragraph(string, font)
+      result = new Paragraph(leading, string, font)
     }
-    return new Paragraph(string)
+    else if (attributes.containsKey("leading") && attributes.containsKey("chunk")) {
+      float leading = attributes.remove("leading")
+      Chunk chunk = attributes.remove("chunk")
+      result = new Paragraph(leading, chunk)
+    }
+    else if (attributes.containsKey("leading") && attributes.containsKey("string")) {
+      float leading = attributes.remove("leading")
+      String string = attributes.remove("string")
+      result = new Paragraph(leading, string)
+    }
+    else if (attributes.containsKey("string") && attributes.containsKey("font")) {
+      String string = attributes.remove("string")
+      Font font = attributes.remove("font")
+      result = new Paragraph(string, font)
+    }
+    else if (attributes.containsKey("leading")) {
+      float leading = attributes.remove("leading")
+      result = new Paragraph(leading)
+    }
+    else if (attributes.containsKey("string")) {
+      String string = attributes.remove("string")
+      result = new Paragraph(string)
+    }
+    else if (attributes.containsKey("chunk")) {
+      Chunk chunk = attributes.remove("chunk")
+      result = new Paragraph(chunk)
+    }
+    else if (attributes.containsKey("phrase")) {
+      Phrase phrase = attributes.remove("phrase")
+      result = new Paragraph(phrase)
+    }
+    else {
+      result = new Paragraph(value)
+    }
+    attributes.each {key, val ->
+      result[key] = val
+    }
+    return result
   }
 
 }
--- src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/PDFBuilderTest.groovy
+++ src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/PDFBuilderTest.groovy
@@ -13,7 +13,8 @@ class PDFBuilderTest {
     def document = new PDFBuilder(outputStream: new File("tryme.pdf").newOutputStream()).document() {
       image(filename: "images/LowagieBook.png")
       paragraph(string: "First paragraph")
-      paragraph("Second paragraph", font: headingFont)
+      paragraph(string: "Second paragraph", font: headingFont)
+      paragraph("Third paragraph")
       table(2, spacingBefore: 15, spacingAfter: 30) {
         cell(string: "cell 1,1"); cell(string: "cell 1,2")
         cell("cell 2,1"); cell("cell 2,2", font: boldFont)
@@ -23,6 +24,8 @@ class PDFBuilderTest {
         cell("more cells") ; cell("20") ; cell("more and more")
         cell("finally ...") ; cell("30") ; cell("the end")
       }
+      paragraph(leading: 10.0f, string: "Leading: 10")
+      paragraph(leading: 15.0f, string: "Leading: 15", font: boldFont)
     }
     assert document.class == Document
   }

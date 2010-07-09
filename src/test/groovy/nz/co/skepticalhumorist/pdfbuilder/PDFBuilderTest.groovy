28dff2f41b4b3f5cded272b49e2ee7b620df0219
John Hurst <john.b.hurst@gmail.com>
2010-07-09 19:36:45 +1200

    Extracted fonts to variables.

--- src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/PDFBuilderTest.groovy
+++ src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/PDFBuilderTest.groovy
@@ -8,13 +8,15 @@ import com.itextpdf.text.Font
 class PDFBuilderTest {
   @Test
   void testOk() {
+    def headingFont = new Font(Font.FontFamily.HELVETICA, 16.0f, Font.BOLD, BaseColor.BLACK)
+    def boldFont = new Font(Font.FontFamily.HELVETICA, 10.0f, Font.BOLD, BaseColor.BLUE)
     def document = new PDFBuilder(outputStream: new File("tryme.pdf").newOutputStream()).document() {
       image(filename: "images/LowagieBook.png")
       paragraph(string: "First paragraph")
-      paragraph("Second paragraph", font: new Font(Font.FontFamily.HELVETICA, 16.0f, Font.BOLD, BaseColor.BLACK))
+      paragraph("Second paragraph", font: headingFont)
       table(numColumns: 2) {
         cell(string: "cell 1,1"); cell(string: "cell 1,2")
-        cell("cell 2,1"); cell("cell 2,2", font: new Font(Font.FontFamily.HELVETICA, 10.0f, Font.BOLD, BaseColor.BLUE))
+        cell("cell 2,1"); cell("cell 2,2", font: boldFont)
       }
     }
     assert document.class == Document

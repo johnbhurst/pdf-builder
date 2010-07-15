ef9e8340f439a7a96ff7aa8b94fbb395c19c3261
John Hurst <john.b.hurst@gmail.com>
2010-07-15 14:00:25 +1200

    Renamed constants to UPPER_CASE.

--- src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/PDFBuilderTest.groovy
+++ src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/PDFBuilderTest.groovy
@@ -8,16 +8,16 @@ import com.itextpdf.text.Font
 class PDFBuilderTest {
   @Test
   void testOk() {
-    def headingFont = new Font(Font.FontFamily.HELVETICA, 16.0f, Font.BOLD, BaseColor.BLACK)
-    def boldFont = new Font(Font.FontFamily.HELVETICA, 10.0f, Font.BOLD, BaseColor.BLUE)
+    def HEADING_FONT = new Font(Font.FontFamily.HELVETICA, 16.0f, Font.BOLD, BaseColor.BLACK)
+    def BOLD_FONT = new Font(Font.FontFamily.HELVETICA, 10.0f, Font.BOLD, BaseColor.BLUE)
     def document = new PDFBuilder(outputStream: new File("tryme.pdf").newOutputStream()).document() {
       image(filename: "images/LowagieBook.png")
       paragraph(string: "First paragraph")
-      paragraph(string: "Second paragraph", font: headingFont)
+      paragraph(string: "Second paragraph", font: HEADING_FONT)
       paragraph("Third paragraph")
       table(2, spacingBefore: 15, spacingAfter: 30) {
         cell(paragraph("cell 1,1")); cell(paragraph("cell 1,2"))
-        cell(paragraph("cell 2,1")); cell(paragraph("cell 2,2", font: boldFont))
+        cell(paragraph("cell 2,1")); cell(paragraph("cell 2,2", font: BOLD_FONT))
       }
       table([0.4f, 0.2f, 0.4f] as float[], spacingAfter: 30) {
         cell(paragraph("wider cell")) ; cell(paragraph("10")) ; cell(paragraph("wider cell"))
@@ -25,9 +25,9 @@ class PDFBuilderTest {
         cell(paragraph("finally ...")) ; cell(paragraph("30")) ; cell(paragraph("the end"))
       }
       paragraph(leading: 10.0f, string: "Leading: 10")
-      paragraph(leading: 15.0f, string: "Leading: 15", font: boldFont)
+      paragraph(leading: 15.0f, string: "Leading: 15", font: BOLD_FONT)
       table([0.5f, 0.5f] as float[]) {
-        cell(paragraph("one", font: boldFont)) ; cell(paragraph("two"))
+        cell(paragraph("one", font: BOLD_FONT)) ; cell(paragraph("two"))
       }
     }
     assert document.class == Document

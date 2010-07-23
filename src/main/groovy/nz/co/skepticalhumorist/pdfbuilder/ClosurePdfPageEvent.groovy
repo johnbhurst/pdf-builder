718137e491ce25fe91ade81de8f16c8f45f4ed11
John Hurst <john.b.hurst@gmail.com>
2010-07-23 16:29:08 +1200

    Fixed PageEvent handling when closures are not given for all events (which is almost always the case!!!).

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ClosurePdfPageEvent.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ClosurePdfPageEvent.groovy
@@ -8,60 +8,60 @@ import com.itextpdf.text.Rectangle
 
 class ClosurePdfPageEvent implements PdfPageEvent {
 
-  Closure onOpenDocument = {}
-  Closure onStartPage = {}
-  Closure onEndPage = {}
-  Closure onCloseDocument = {}
-  Closure onParagraph = {}
-  Closure onParagraphEnd = {}
-  Closure onChapter = {}
-  Closure onChapterEnd = {}
-  Closure onSection = {}
-  Closure onSectionEnd = {}
-  Closure onGenericTag = {}
+  Closure onOpenDocument
+  Closure onStartPage
+  Closure onEndPage
+  Closure onCloseDocument
+  Closure onParagraph
+  Closure onParagraphEnd
+  Closure onChapter
+  Closure onChapterEnd
+  Closure onSection
+  Closure onSectionEnd
+  Closure onGenericTag
 
   void onOpenDocument(PdfWriter writer, Document document) {
-    onOpenDocument.call(writer, document)
+    if (onOpenDocument) onOpenDocument.call(writer, document)
   }
 
   void onStartPage(PdfWriter writer, Document document) {
-    onStartPage.call(writer, document)
+    if (onStartPage) onStartPage.call(writer, document)
   }
 
   void onEndPage(PdfWriter writer, Document document) {
-    onEndPage.call(writer, document)
+    if (onEndPage) onEndPage.call(writer, document)
   }
 
   void onCloseDocument(PdfWriter writer, Document document) {
-    onCloseDocument.call(writer, document)
+    if (onCloseDocument) onCloseDocument.call(writer, document)
   }
 
   void onParagraph(PdfWriter writer, Document document, float paragraphPosition) {
-    onParagraph.call(writer,  document, paragraphPosition)
+    if (onParagraph) onParagraph.call(writer,  document, paragraphPosition)
   }
 
   void onParagraphEnd(PdfWriter writer, Document document, float paragraphPosition) {
-    onParagraphEnd.call(writer, document, paragraphPosition)
+    if (onParagraphEnd) onParagraphEnd.call(writer, document, paragraphPosition)
   }
 
   void onChapter(PdfWriter writer, Document document, float paragraphPosition, Paragraph title) {
-    onChapter.call(writer, document, paragraphPosition, title)
+    if (onChapter) onChapter.call(writer, document, paragraphPosition, title)
   }
 
   void onChapterEnd(PdfWriter writer, Document document, float paragraphPosition) {
-    onChapterEnd.call(writer, document, paragraphPosition)
+    if (onChapterEnd) onChapterEnd.call(writer, document, paragraphPosition)
   }
 
   void onSection(PdfWriter writer, Document document, float paragraphPosition, int depth, Paragraph title) {
-    onSection.call(writer, document, paragraphPosition, depth, title)
+    if (onSection) onSection.call(writer, document, paragraphPosition, depth, title)
   }
 
   void onSectionEnd(PdfWriter writer, Document document, float paragraphPosition) {
-    onSectionEnd.call(writer, document, paragraphPosition)
+    if (onSectionEnd) onSectionEnd.call(writer, document, paragraphPosition)
   }
 
   void onGenericTag(PdfWriter writer, Document document, Rectangle rect, String text) {
-    onGenericTag.call(writer, document, rect, text)
+    if (onGenericTag) onGenericTag.call(writer, document, rect, text)
   }
 
 }

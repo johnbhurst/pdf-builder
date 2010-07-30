7cbfd767dfc2d87c0816322f0bdd19b5315835fc
John Hurst <john.b.hurst@gmail.com>
2010-07-31 11:56:16 +1200

    Added withDirectContentUnder().

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/PdfWriterCategory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/PdfWriterCategory.groovy
@@ -14,4 +14,15 @@ class PdfWriterCategory {
       cb.restoreState()
     }
   }
+
+  public static void withDirectContentUnder(PdfWriter writer, Closure closure) {
+    PdfContentByte cb = writer.getDirectContentUnder()
+    cb.saveState()
+    try {
+      closure.call(cb)
+    }
+    finally {
+      cb.restoreState()
+    }
+  }
 }

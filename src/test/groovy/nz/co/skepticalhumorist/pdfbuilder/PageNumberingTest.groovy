6bcab9d15f68bace5d5635708b3ad36e25699283
John Hurst <john.b.hurst@gmail.com>
2010-07-31 11:33:35 +1200

    Try to get page numbering working using PdfPageLabels in writer initialization. This doesn't seem to work.

---
+++ src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/PageNumberingTest.groovy
@@ -0,0 +1,21 @@
+package nz.co.skepticalhumorist.pdfbuilder
+
+import org.junit.Test
+import com.itextpdf.text.pdf.PdfPageLabels
+
+class PageNumberingTest {
+  @Test
+  void testOk() {
+    new PDFBuilder(outputStream: new File("PageNumberingTest.pdf").newOutputStream()).document(
+      initWriter: {
+        PdfPageLabels labels = new PdfPageLabels()
+        labels.addPageLabel(1, PdfPageLabels.DECIMAL_ARABIC_NUMERALS)
+        setPageLabels(labels)
+      }
+    ) {
+      100.times {i ->
+        paragraph("Paragraph $i.")
+      }
+    }
+  }
+}

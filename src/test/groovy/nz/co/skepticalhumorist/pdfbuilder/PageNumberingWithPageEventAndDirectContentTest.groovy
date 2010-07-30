45e2e88c514d4335cd486ca35f3557d11b1c45f1
John Hurst <john.b.hurst@gmail.com>
2010-07-31 11:53:46 +1200

    Added PageNumberingWithPageEventAndDirectContentTest. This one works.

---
+++ src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/PageNumberingWithPageEventAndDirectContentTest.groovy
@@ -0,0 +1,31 @@
+package nz.co.skepticalhumorist.pdfbuilder
+
+import org.junit.Test
+import com.itextpdf.text.pdf.PdfContentByte
+import com.itextpdf.text.pdf.BaseFont
+import com.itextpdf.text.pdf.PdfDocument
+import com.itextpdf.text.pdf.PdfWriter
+
+class PageNumberingWithPageEventAndDirectContentTest {
+
+  BaseFont font = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.WINANSI, BaseFont.NOT_EMBEDDED)
+
+  @Test
+  public void testOk() {
+    new PDFBuilder(outputStream: new File("PageNumberingWithPageEventAndDirectContentTest.pdf").newOutputStream()).document(
+      onEndPage: {PdfWriter writer, PdfDocument document ->
+        writer.withDirectContent {PdfContentByte cb ->
+          cb.beginText()
+          cb.setFontAndSize(font, 12)
+          cb.setTextMatrix(document.left() as float, document.bottom()- 20 as float)
+          cb.showText("Page " + writer.getPageNumber())
+          cb.endText()
+        }
+      }
+    ) {
+      100.times {i ->
+        paragraph("Paragraph $i.")
+      }
+    }
+  }
+}

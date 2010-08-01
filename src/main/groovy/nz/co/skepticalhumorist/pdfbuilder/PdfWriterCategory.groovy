21ffa9db499fd93816806677c6bb5187538cb652
John Hurst <john.b.hurst@gmail.com>
2010-08-02 05:29:56 +1200

    Extracted common code to method.

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/PdfWriterCategory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/PdfWriterCategory.groovy
@@ -5,18 +5,14 @@ import com.itextpdf.text.pdf.PdfContentByte
 
 class PdfWriterCategory {
   public static void withDirectContent(PdfWriter writer, Closure closure) {
-    PdfContentByte cb = writer.getDirectContent()
-    cb.saveState()
-    try {
-      closure.call(cb)
-    }
-    finally {
-      cb.restoreState()
-    }
+    doWithContentByte(writer.directContent, closure)
   }
 
   public static void withDirectContentUnder(PdfWriter writer, Closure closure) {
-    PdfContentByte cb = writer.getDirectContentUnder()
+    doWithContentByte(writer.directContentUnder, closure)
+  }
+
+  private static void doWithContentByte(PdfContentByte cb, Closure closure) {
     cb.saveState()
     try {
       closure.call(cb)

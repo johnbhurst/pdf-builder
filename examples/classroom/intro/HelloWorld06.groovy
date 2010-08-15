e2f1e6151a11a6503bbce3430fd267f4d6d2477c
John Hurst <john.b.hurst@gmail.com>
2010-08-16 11:07:51 +1200

    Added HelloWorld06 example.

---
+++ examples/classroom/intro/HelloWorld06.groovy
@@ -0,0 +1,24 @@
+@Grab("nz.co.skepticalhumorist:pdf-builder:latest.release")
+import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
+import com.lowagie.text.pdf.BaseFont
+
+def RESULT = "build/examples/classroom/intro/hello06.pdf"
+BaseFont bf = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+new PDFBuilder(new FileOutputStream(RESULT)).document(
+  // This example uses the onOpenDocument event hook to write with the DirectContent layer.
+  // It would be better if this layer could be accessed via the main builder block.
+  // I.e., it would be more analogous to the iText example.
+  // But on the other hand, accessing DirectContent via event hooks might be a more typicaly real use-case.
+  onOpenDocument: {writer, document ->
+    writer.directContent.with {cb ->
+      cb.beginText(); // BT
+      cb.moveText(36, 806); // 36 806 Td
+      cb.moveText(0, -18); // 0 -18 Td
+      cb.setFontAndSize(bf, 12); // /F1 12 Tf
+      cb.showText("Hello World"); // (Hello World)Tj
+      cb.endText(); // ET
+    }
+  }
+) {
+  // nothing in main
+}

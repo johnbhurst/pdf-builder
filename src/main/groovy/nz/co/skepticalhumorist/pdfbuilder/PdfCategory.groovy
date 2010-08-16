01b127e7d4293f241c6a26f5afad91b14c65361c
John Hurst <john.b.hurst@gmail.com>
2010-08-16 14:11:33 +1200

    Added HelloWorld10a (Graphics2D) example, with some PdfContentByte+Graphics2D support.

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/PdfCategory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/PdfCategory.groovy
@@ -2,6 +2,8 @@ package nz.co.skepticalhumorist.pdfbuilder
 
 import com.lowagie.text.pdf.PdfWriter
 import com.lowagie.text.pdf.PdfContentByte
+import java.awt.Graphics2D
+import com.lowagie.text.pdf.FontMapper
 
 class PdfCategory {
   static void withDirectContent(PdfWriter writer, Closure closure) {
@@ -10,6 +12,44 @@ class PdfCategory {
 
   static void withDirectContentUnder(PdfWriter writer, Closure closure) {
     doWithContentByte(writer.directContentUnder, closure)
+  }
+
+  static void withDirectContentGraphics(PdfWriter writer, float width, float height, Closure closure) {
+    doWithContentByte(writer.directContent) {PdfContentByte cb ->
+      doWithContentByteAndGraphics(cb, cb.createGraphics(width, height), closure)
+    }
+  }
+
+  static void withDirectContentGraphics(PdfWriter writer, float width, float height, FontMapper fontMapper, Closure closure) {
+    doWithContentByte(writer.directContent) {PdfContentByte cb ->
+      doWithContentByteAndGraphics(cb, cb.createGraphics(width, height, fontMapper), closure)
+    }
+  }
+
+  static void withDirectContentGraphics(PdfWriter writer, float width, float height, boolean convertImagesToJPEG, float quality, Closure closure) {
+    doWithContentByte(writer.directContent) {PdfContentByte cb ->
+      doWithContentByteAndGraphics(cb, cb.createGraphics(width, height, convertImagesToJPEG, quality), closure)
+    }
+  }
+
+  static void withDirectContentGraphics(PdfWriter writer, float width, float height, FontMapper fontMapper, boolean convertImagesToJPEG, float quality, Closure closure) {
+    doWithContentByte(writer.directContent) {PdfContentByte cb ->
+      doWithContentByteAndGraphics(cb, cb.createGraphics(width, height, fontMapper, convertImagesToJPEG, quality), closure)
+    }
+  }
+
+  private static void doWithContentByteAndGraphics(PdfContentByte cb, Graphics2D graphics, Closure closure) {
+    try {
+      if (closure.maximumNumberOfParameters > 1) {
+        closure.call(cb, graphics)
+      }
+      else {
+        closure.call(graphics)
+      }
+    }
+    finally {
+      graphics.dispose()
+    }
   }
 
   private static void doWithContentByte(PdfContentByte cb, Closure closure) {

1445a2971f8aa5f26ff97b360d244c5133dce922
John Hurst <john.b.hurst@gmail.com>
2010-08-23 14:28:38 +1200

    Reverted some changes; chunks don't work yet.

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ChunkFactory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ChunkFactory.groovy
@@ -2,7 +2,7 @@ package nz.co.skepticalhumorist.pdfbuilder
 
 import com.lowagie.text.Chunk
 
-class ChunkFactory extends AbstractDocumentChildFactory {
+class ChunkFactory extends AbstractFactory {
 
   Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
     return new Chunk(value);

5b59061ad54c2294ea89b788fff268b6dfef0fec
John Hurst <john.b.hurst@gmail.com>
2010-08-27 08:28:06 +1200

    Added constructFromAttributes() to improve handling of argument combinations for Paragraph constructor.

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/AbstractElementFactory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/AbstractElementFactory.groovy
@@ -4,6 +4,27 @@ import com.lowagie.text.pdf.PdfPCell
 import com.lowagie.text.pdf.PdfPTable
 
 abstract class AbstractElementFactory extends AbstractFactory {
+
+  def constructFromAttributes(Class cls, List<Map<String, Class>> constructors, Map<String, Object> attributes, Object value) {
+    constructors.each {ctorMap ->
+      def ctorAttrNames = ctorMap.keySet()
+      if (ctorAttrNames.every {k -> attributes.containsKey(k)}) {
+        def args = ctorAttrNames.collect {k -> attributes.remove(k)}
+        return cls.newInstance(*args)
+      }
+    }
+    if (value && value instanceof GString) {
+      // why do we need to treat this specially?
+      return cls.newInstance(value.toString())
+    }
+    else if (value) {
+      return cls.newInstance(value)
+    }
+    else {
+      return cls.newInstance()
+    }
+  }
+
   @Override
   void onNodeCompleted(FactoryBuilderSupport builder, Object parent, Object node) {
     switch (parent) {
--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ParagraphFactory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ParagraphFactory.groovy
@@ -7,48 +7,19 @@ import com.lowagie.text.Phrase
 
 class ParagraphFactory extends AbstractElementFactory {
 
+  def constructors = [
+    [leading: float, string: String, font: Font],
+    [leading: float, chunk: Chunk],
+    [leading: float, string: String],
+    [string: String, font: Font],
+    [leading: float],
+    [string: String],
+    [chunk: Chunk],
+    [phrase: Phrase]
+  ]
+
   Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
-    Paragraph result = null
-    if (attributes.containsKey("leading") && attributes.containsKey("string") && attributes.containsKey("font")) {
-      float leading = attributes.remove("leading")
-      String string = attributes.remove("string")
-      Font font = attributes.remove("font")
-      result = new Paragraph(leading, string, font)
-    }
-    else if (attributes.containsKey("leading") && attributes.containsKey("chunk")) {
-      float leading = attributes.remove("leading")
-      Chunk chunk = attributes.remove("chunk")
-      result = new Paragraph(leading, chunk)
-    }
-    else if (attributes.containsKey("leading") && attributes.containsKey("string")) {
-      float leading = attributes.remove("leading")
-      String string = attributes.remove("string")
-      result = new Paragraph(leading, string)
-    }
-    else if (attributes.containsKey("string") && attributes.containsKey("font")) {
-      String string = attributes.remove("string")
-      Font font = attributes.remove("font")
-      result = new Paragraph(string, font)
-    }
-    else if (attributes.containsKey("leading")) {
-      float leading = attributes.remove("leading")
-      result = new Paragraph(leading)
-    }
-    else if (attributes.containsKey("string")) {
-      String string = attributes.remove("string")
-      result = new Paragraph(string)
-    }
-    else if (attributes.containsKey("chunk")) {
-      Chunk chunk = attributes.remove("chunk")
-      result = new Paragraph(chunk)
-    }
-    else if (attributes.containsKey("phrase")) {
-      Phrase phrase = attributes.remove("phrase")
-      result = new Paragraph(phrase)
-    }
-    else {
-      result = new Paragraph(value)
-    }
+    Paragraph result = constructFromAttributes(Paragraph, constructors, attributes, value)
     attributes.each {key, val ->
       result[key] = val
     }

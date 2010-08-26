4a451ab10f38eecb0639d9062a0ffbeb98f3e9ef
John Hurst <john.b.hurst@gmail.com>
2010-08-27 11:17:31 +1200

    Refined ChunkFactory to use argsFromAttributes(), now supports all Chunk constructors.

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ChunkFactory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ChunkFactory.groovy
@@ -1,11 +1,34 @@
 package nz.co.skepticalhumorist.pdfbuilder
 
 import com.lowagie.text.Chunk
+import com.lowagie.text.Font
+import com.lowagie.text.Image
+import com.lowagie.text.pdf.draw.DrawInterface
+import org.codehaus.groovy.runtime.InvokerHelper
 
 class ChunkFactory extends AbstractElementFactory {
 
+  def static ctorArgTypes = [
+    [chunk: Chunk],
+    [content: String, font: Font],
+    [content: String],
+    [c: char, font: Font],
+    [c: char],
+    [image: Image, offsetX: float, offsetY: float, changeLeading: boolean],
+    [image: Image, offsetX: float, offsetY: float],
+    [separator: DrawInterface, tabPosition: float, newline: boolean],
+    [separator: DrawInterface, tabPosition: float],
+    [separator: DrawInterface, vertical: boolean],
+    [separator: DrawInterface],
+  ]
+
   Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
-    return new Chunk(value);
+    def ctorArgs = PDFBuilder.argsFromAttributes(ctorArgTypes, attributes, value)
+    Chunk result = InvokerHelper.invokeConstructorOf(Chunk, ctorArgs)
+    attributes.each {key, val ->
+      result[key] = val
+    }
+    return result
   }
 
 }

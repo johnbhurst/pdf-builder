42688094a32f41f0c8ff7a049f7bbb0ce3d7b394
John Hurst <john.b.hurst@gmail.com>
2010-08-26 21:20:44 +1200

    Use more logical index names for row/col indices.

--- examples/questions/separators/PascalsTriangle.groovy
+++ examples/questions/separators/PascalsTriangle.groovy
@@ -2,21 +2,21 @@
 import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
 import com.lowagie.text.pdf.draw.VerticalPositionMark;
 
-pascal = [:].withDefault {k ->
-  [:].withDefault {i ->
-    if (k == 0 || k == 1) 1
-    else if (i == 0 || i == k) 1
-    else pascal[k-1][i-1] + pascal[k-1][i]
+pascal = [:].withDefault {i ->
+  [:].withDefault {j ->
+    if (i == 0 || i == 1) 1
+    else if (j == 0 || j == i) 1
+    else pascal[i-1][j-1] + pascal[i-1][j]
   }
 }
 def vp = new VerticalPositionMark()
 
 def RESULT = "build/examples/questions/separators/PascalsTriangle.pdf"
 new PDFBuilder(new FileOutputStream(RESULT)).document() {
-  for (k in 0..20) {
+  for (i in 0..20) {
     paragraph(chunk(vp)) {
-      for (i in 0..k) {
-        chunk("" + pascal[k][i])
+      for (j in 0..i) {
+        chunk("" + pascal[i][j])
         chunk(vp)
       }
     }

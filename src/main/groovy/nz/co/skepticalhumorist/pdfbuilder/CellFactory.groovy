1b0b19b9e5d0a632e7ac251d3e0e8a1d7d8ef217
John Hurst <john.b.hurst@gmail.com>
2010-08-27 11:07:12 +1200

    Refined CellFactory to use argsFromAttributes(), now supports all PdfPCell constructors.

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/CellFactory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/CellFactory.groovy
@@ -1,11 +1,29 @@
 package nz.co.skepticalhumorist.pdfbuilder
 
 import com.lowagie.text.pdf.PdfPCell
+import com.lowagie.text.Image
+import com.lowagie.text.Phrase
+import com.lowagie.text.pdf.PdfPTable
+import org.codehaus.groovy.runtime.InvokerHelper
 
 class CellFactory extends AbstractElementFactory {
 
+  def static ctorArgTypes = [
+    [phrase: Phrase],
+    [image: Image, fit: boolean],
+    [image: Image],
+    [table: PdfPTable, style: PdfPCell],
+    [table: PdfPTable],
+    [cell: PdfPCell]
+  ]
+
   Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
-    return new PdfPCell(value)
+    def ctorArgs = PDFBuilder.argsFromAttributes(ctorArgTypes, attributes, value)
+    PdfPCell result = InvokerHelper.invokeConstructorOf(PdfPCell, ctorArgs)
+    attributes.each {key, val ->
+      result[key] = val
+    }
+    return result
   }
 
 }

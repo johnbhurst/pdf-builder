77784b7a9de64aaa6286de7ae5aabeebb8bb8fc7
John Hurst <john.b.hurst@gmail.com>
2010-08-27 11:06:47 +1200

    Ordered imports.

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ParagraphFactory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/ParagraphFactory.groovy
@@ -1,8 +1,8 @@
 package nz.co.skepticalhumorist.pdfbuilder
 
+import com.lowagie.text.Chunk
 import com.lowagie.text.Font
 import com.lowagie.text.Paragraph
-import com.lowagie.text.Chunk
 import com.lowagie.text.Phrase
 import org.codehaus.groovy.runtime.InvokerHelper
 

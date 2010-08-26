fef087dce62f8d140673f3c1455ee18eb4586309
John Hurst <john.b.hurst@gmail.com>
2010-08-27 11:21:51 +1200

    Refined TableFactory to use argsFromAttributes(), now supports all PdfPTable constructors.

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/TableFactory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/TableFactory.groovy
@@ -1,33 +1,23 @@
 package nz.co.skepticalhumorist.pdfbuilder
 
-import com.lowagie.text.pdf.PdfPCell
 import com.lowagie.text.pdf.PdfPTable
+import org.codehaus.groovy.runtime.InvokerHelper
 
 class TableFactory extends AbstractElementFactory {
 
+  def static ctorArgTypes = [
+    [relativeWidths: float[]],
+    [numColumns: int],
+    [table: PdfPTable]
+  ]
+
   Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
-    PdfPTable result = new PdfPTable(value)
+    def ctorArgs = PDFBuilder.argsFromAttributes(ctorArgTypes, attributes, value)
+    PdfPTable result = InvokerHelper.invokeConstructorOf(PdfPTable, ctorArgs)
     attributes.each {key, val ->
       result[key] = val
     }
     return result
   }
-
-  def void setParent(FactoryBuilderSupport builder, Object parent, Object child) {
-    //println("TableFactory.setParent($builder, $parent, $child)") // Add DEBUG message?
-//    parent.addCell(child)
-  }
-
-  def void setChild(FactoryBuilderSupport builder, Object parent, Object child) {
-    //println("TableFactory.setChild($builder, $parent, (${child.class}) $child)") // Add DEBUG message?
-//    if (child.class == PdfPCell) {
-//      parent.addCell(child)
-//    }
-  }
-
-//  def void onNodeCompleted(FactoryBuilderSupport builder, Object parent, Object node) {
-    //println("TableFactory.onNodeCompleted($builder, $parent, $node)") // Add DEBUG message?
-//    parent.add(node)
-//  }
 
 }

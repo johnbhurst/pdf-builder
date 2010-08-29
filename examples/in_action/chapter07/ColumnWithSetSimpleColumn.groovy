7c738f2ae626af3d3a936a63debf42f0829b96ab
John Hurst <john.b.hurst@gmail.com>
2010-08-29 19:32:47 +1200

    Added ColumnWithSetSimpleColumn example from iText In Action book.

---
+++ examples/in_action/chapter07/ColumnWithSetSimpleColumn.groovy
@@ -0,0 +1,20 @@
+@Grab("nz.co.skepticalhumorist:pdf-builder:latest.release")
+import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
+import com.lowagie.text.Element
+import com.lowagie.text.PageSize
+import com.lowagie.text.Phrase
+import com.lowagie.text.pdf.ColumnText
+
+def RESULT= "build/examples/in_action/chapter07/ColumnWithSetSimpleColumn.pdf"
+def INPUT = System.getProperty("itext.examples.home") + "/resources/in_action/chapter07/caesar.txt"
+new PDFBuilder(new FileOutputStream(RESULT)).document(PageSize.A4) {
+  String text = new File(INPUT).text
+  ColumnText ct = new ColumnText(writer.directContent)
+  ct.setSimpleColumn(new Phrase(text), 36, 36, PageSize.A4.width - 36 as float, PageSize.A4.height - 36 as float, 18, Element.ALIGN_JUSTIFIED)
+  int status = ColumnText.START_COLUMN
+  while (ColumnText.hasMoreText(status)) {
+    status = ct.go()
+    ct.setYLine(PageSize.A4.height - 36 as float)
+    document.newPage()
+  }
+}

bfe124c2f41436aa93815df545ddecad957515e7
John Hurst <john.b.hurst@gmail.com>
2010-08-31 20:42:29 +1200

    Added heading table cell spanning three columns.

--- src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/TableTest.groovy
+++ src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/TableTest.groovy
@@ -2,14 +2,16 @@ package nz.co.skepticalhumorist.pdfbuilder
 
 import org.junit.Test
 import com.lowagie.text.Paragraph
+import com.lowagie.text.Element
 
 class TableTest extends AbstractPDFBuilderTestCase {
   @Test
   void testOk() {
     defaultBuilder.document() {
-      table(2, spacingBefore: 15, spacingAfter: 30) {
-        cell(new Paragraph("cell 1,1")); cell(new Paragraph("cell 1,2"))
-        cell(new Paragraph("cell 2,1")); cell(new Paragraph("cell 2,2"))
+      table(3, spacingBefore: 15, spacingAfter: 30) {
+        cell(colspan: 3) {paragraph(string: "Table Heading", alignment: Element.ALIGN_CENTER)}
+        cell(new Paragraph("cell 1,1")); cell(new Paragraph("cell 1,2")); cell(new Paragraph("cell 1,3"))
+        cell(new Paragraph("cell 2,1")); cell(new Paragraph("cell 2,2")); cell(new Paragraph("cell 2,3"))
       }
     }
   }

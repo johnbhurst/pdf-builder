aa272a246d32c80ceabc8918ca01cfb2f764b2c3
John Hurst <john.b.hurst@gmail.com>
2010-09-04 19:53:57 +1200

    Added PdfPTableVerticalCells example from iText in Action book.

---
+++ examples/in_action/chapter06/PdfPTableVerticalCells.groovy
@@ -0,0 +1,16 @@
+@Grab("nz.co.skepticalhumorist:pdf-builder:latest.release")
+import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
+import java.awt.Color
+import com.lowagie.text.Element
+import com.lowagie.text.PageSize
+import com.lowagie.text.Paragraph
+
+def RESULT= "build/examples/in_action/chapter06/PdfPTableVerticalCells.pdf"
+new PDFBuilder(new FileOutputStream(RESULT)).document() {
+  table([1, 4] as float[], widthPercentage: 30) {
+    cell(new Paragraph("fox"), padding: 4, backgroundColor: Color.YELLOW, horizontalAlignment: Element.ALIGN_CENTER, rotation: 90)
+    current.addCell("The fox is a red/brown animal that is very quick and that jumps over dogs.")
+    cell(new Paragraph("dog"), padding: 4, backgroundColor: Color.YELLOW, horizontalAlignment: Element.ALIGN_CENTER, rotation: 90)
+    current.addCell("The dog in the sentence 'quick brown fox jumps over the lazy dog' is a rather lazy animal.")
+  }
+}

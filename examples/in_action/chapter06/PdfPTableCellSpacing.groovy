8aa3595b1eaf4d072ccee52a3afaeddc62970e07
John Hurst <john.b.hurst@gmail.com>
2010-09-04 17:08:07 +1200

    Optimized imports.

--- examples/in_action/chapter06/PdfPTableCellSpacing.groovy
+++ examples/in_action/chapter06/PdfPTableCellSpacing.groovy
@@ -1,10 +1,8 @@
 @Grab("nz.co.skepticalhumorist:pdf-builder:latest.release")
 import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
 import com.lowagie.text.Paragraph
-import com.lowagie.text.Element
-import com.lowagie.text.pdf.PdfPTable
-import com.lowagie.text.pdf.PdfPCell
 import com.lowagie.text.Phrase
+import com.lowagie.text.pdf.PdfPCell
 
 def RESULT= "build/examples/in_action/chapter06/PdfPTableCellSpacing.pdf"
 new PDFBuilder(new FileOutputStream(RESULT)).document() {

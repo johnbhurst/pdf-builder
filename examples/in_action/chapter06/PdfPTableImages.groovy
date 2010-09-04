7606766bb9026c5c9b5e8f269ff08cec577617f3
John Hurst <john.b.hurst@gmail.com>
2010-09-04 20:14:01 +1200

    Added PdfPTableImages example from iText in Action book.

---
+++ examples/in_action/chapter06/PdfPTableImages.groovy
@@ -0,0 +1,17 @@
+@Grab("nz.co.skepticalhumorist:pdf-builder:latest.release")
+import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
+import com.lowagie.text.Image
+
+def RESULT= "build/examples/in_action/chapter06/PdfPTableImages.pdf"
+new PDFBuilder(new FileOutputStream(RESULT)).document() {
+  table(1) {
+    def table = current
+    Image img = Image.getInstance(System.getProperty("itext.examples.home") + "/resources/in_action/chapter06/foxdog.jpg")
+    table.addCell("This image was added with addCell(Image); the image is scaled + there is the default padding of getDefaultCell.")
+    table.addCell(img);
+    table.addCell("This image was added with addCell(PdfPCell); scaled, no padding")
+    cell(image: img, fit: true)
+    table.addCell("This image was added with addCell(PdfPCell); not scaled")
+    cell(image: img, fit: false)
+  }
+}

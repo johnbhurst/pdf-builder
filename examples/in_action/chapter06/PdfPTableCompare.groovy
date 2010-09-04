5d73d0062bbbd6e9725a9f3f7072a14f7b1a6af0
John Hurst <john.b.hurst@gmail.com>
2010-09-05 08:48:13 +1200

    Added PdfPTableCompare example from iText in Action book.

---
+++ examples/in_action/chapter06/PdfPTableCompare.groovy
@@ -0,0 +1,23 @@
+@Grab("nz.co.skepticalhumorist:pdf-builder:latest.release")
+import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
+import com.lowagie.text.Chunk
+import com.lowagie.text.Element
+import com.lowagie.text.PageSize
+
+def RESULT= "build/examples/in_action/chapter06/PdfPTableCompare.pdf"
+new PDFBuilder(new FileOutputStream(RESULT)).document(PageSize.A4) {
+  paragraph("The table below is added with document.add():")
+  chunk(Chunk.NEWLINE)
+  // Note: below creates the table, adds it to the document, and also stores it in variable for reuse
+  def table = table(3) {
+    current.defaultCell.horizontalAlignment = Element.ALIGN_CENTER
+    current.addCell("the quick brown fox")
+    current.addCell("jumps over")
+    current.addCell("the lazy dog")
+    current.addCell("the lazy dog")
+    current.addCell("jumps over")
+    current.addCell("the quick brown fox")
+  }
+  paragraph("The table below is added with writeSelectedRows() at position (x = 50; y = ${PageSize.A4.height * 0.75}):")
+  table.writeSelectedRows(0, -1, 50, PageSize.A4.height * 0.75 as float, writer.directContent)
+}

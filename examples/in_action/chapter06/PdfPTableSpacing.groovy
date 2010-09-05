607078942745fe982647cf29d785e09ad7de759c
John Hurst <john.b.hurst@gmail.com>
2010-09-05 12:56:19 +1200

    Corrected example to use new Paragraph() in cell constructor call: consistent with original iText example.

--- examples/in_action/chapter06/PdfPTableSpacing.groovy
+++ examples/in_action/chapter06/PdfPTableSpacing.groovy
@@ -1,15 +1,15 @@
 @Grab("nz.co.skepticalhumorist:pdf-builder:latest.release")
 import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
+import com.lowagie.text.Paragraph
 
 def RESULT= "build/examples/in_action/chapter06/PdfPTableSpacing.pdf"
 new PDFBuilder(new FileOutputStream(RESULT)).document() {
   paragraph("We add 2 tables:")
   def t = table(3) {
-    cell(colspan: 3) {paragraph("header with colspan 3")}
+    cell(new Paragraph("header with colspan 3"), colspan: 3)
     current.with {
       addCell("1.1"); addCell("2.1"); addCell("3.1")
       addCell("1.2"); addCell("2.2"); addCell("3.2")
-
     }
   }
   document.add(t)

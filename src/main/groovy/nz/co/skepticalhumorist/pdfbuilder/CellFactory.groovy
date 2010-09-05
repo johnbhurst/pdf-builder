cbb8f53a28493844444953b49fd8e13238c56067
John Hurst <john.b.hurst@gmail.com>
2010-09-05 12:24:07 +1200

    Added comment about CellFactory String special case.

--- src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/CellFactory.groovy
+++ src/main/groovy/nz/co/skepticalhumorist/pdfbuilder/CellFactory.groovy
@@ -21,6 +21,11 @@ class CellFactory extends ElementFactory {
 
   @Override
   Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
+    // If called with a String, this does not match any PdfPCell constructor.
+    // Instead we make a special case of this.
+    // We return the String for PdfPTable.addCell(String), and ignore any other attributes.
+    // No PdfPCell is actually created.  The table's defaultCell is used instead.
+    // Nested content is invalid.
     if (value instanceof String) {
       return value
     }

b359a7441ea5fbda27e5937686a5c453f69fa2c6
John Hurst <john.b.hurst@gmail.com>
2010-09-09 18:16:13 +1200

    Corrected parenthesis placement.

--- examples/in_action/chapter04/FoxDogAnchor1.groovy
+++ examples/in_action/chapter04/FoxDogAnchor1.groovy
@@ -8,5 +8,6 @@ new PDFBuilder(new FileOutputStream(RESULT)).document() {
   anchor(
     string: "Quick brown fox jumps over the lazy dog.",
     font: new Font(Font.UNDEFINED, Font.UNDEFINED, Font.UNDERLINE, new GrayColor(0.3f)),
-    reference: "http://en.wikipedia.org/wiki/The_quick_brown_fox_jumps_over_the_lazy_dog")
+    reference: "http://en.wikipedia.org/wiki/The_quick_brown_fox_jumps_over_the_lazy_dog"
+  )
 }

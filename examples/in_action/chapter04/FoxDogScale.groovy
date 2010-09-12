c57e842ec7a5ba7aab4a0d7d471fd9fbf65582ef
John Hurst <john.b.hurst@gmail.com>
2010-09-13 11:01:43 +1200

    Removed unnecessary semicolon.

--- examples/in_action/chapter04/FoxDogScale.groovy
+++ examples/in_action/chapter04/FoxDogScale.groovy
@@ -5,7 +5,7 @@ import com.lowagie.text.Chunk
 def RESULT= "build/examples/in_action/chapter04/FoxDogScale.pdf"
 new PDFBuilder(new FileOutputStream(RESULT)).document() {
   Chunk c = new Chunk("quick brown fox jumps over the lazy dog")
-  float w = c.widthPoint;
+  float w = c.widthPoint
   paragraph("The width of the chunk: '") {
     current.add(c)
     chunk("' is $w points or ${w/72f as float} inches or ${w/72f*2.54f as float} cm.")

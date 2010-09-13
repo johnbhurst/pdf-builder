b08779c3a7de1a0a005ac9d7d5d6106f3b83ed35
John Hurst <john.b.hurst@gmail.com>
2010-09-13 15:14:56 +1200

    Made chapter 6 examples dependent on itext.examples.home property: one example does use an image from the examples resources directory.

--- src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/ExamplesTest.groovy
+++ src/test/groovy/nz/co/skepticalhumorist/pdfbuilder/ExamplesTest.groovy
@@ -18,10 +18,10 @@ class ExamplesTest {
     runExamples("examples/questions/graphics2D")
     runExamples("examples/questions/separators")
     runExamples("examples/in_action/chapter04")
-    runExamples("examples/in_action/chapter06")
     if (System.properties.containsKey("itext.examples.home")) {
       //println "Running iText examples..."
       runExamples("examples/in_action/chapter05")
+      runExamples("examples/in_action/chapter06")
       runExamples("examples/in_action/chapter07")
     }
   }

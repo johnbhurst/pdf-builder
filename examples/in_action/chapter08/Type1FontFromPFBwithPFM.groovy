eda5898aed620fa1c69ab2d4873f638446690e9b
John Hurst <john.b.hurst@gmail.com>
2010-09-18 10:11:12 +1200

    Added Type1FontFromPFBwithPFM.groovy example from iText in Action book.

---
+++ examples/in_action/chapter08/Type1FontFromPFBwithPFM.groovy
@@ -0,0 +1,14 @@
+@Grab("nz.co.skepticalhumorist:pdf-builder:latest.release")
+import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
+import com.lowagie.text.Font
+import com.lowagie.text.pdf.BaseFont
+
+String resourcePath(String path) {
+  System.properties["itext.examples.home"] + "/resources/in_action/chapter08/" + path
+}
+
+new PDFBuilder(new FileOutputStream("build/examples/in_action/chapter08/Type1FontFromPFBwithPFM.pdf")).document() {
+  BaseFont bf = BaseFont.createFont(resourcePath("cmr10.pfm"), "", BaseFont.EMBEDDED)
+  Font font = new Font(bf, 12)
+  paragraph(string: "0123456789\nabcdefghijklmnopqrstuvwxyz\nABCDEFGHIJKLMNOPQRSTUVWXZ", font: font)
+}

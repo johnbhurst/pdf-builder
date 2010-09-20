79b84784ae4f6f647d9c5bac5f1b23656e44128b
John Hurst <john.b.hurst@gmail.com>
2010-09-20 12:59:40 +1200

    Added GraphicsStateStack.groovy example from iText in Action book.

---
+++ examples/in_action/chapter10/GraphicsStateStack.groovy
@@ -0,0 +1,24 @@
+@Grab("nz.co.skepticalhumorist:pdf-builder:latest.release")
+import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
+import java.awt.Color
+
+new PDFBuilder(new FileOutputStream("build/examples/in_action/chapter10/GraphicsStateStack.pdf")).document() {
+  writer.directContent.withContentByte {cb1 ->
+    cb1.circle(260, 500, 250)
+    cb1.fill()
+    cb1.withContentByte {cb2 ->
+      cb2.setColorFill(Color.yellow)
+      cb2.circle(260, 500, 200)
+      cb2.fill()
+      cb2.withContentByte {cb3 ->
+        cb3.setColorFill(Color.red)
+        cb3.circle(260, 500, 150)
+        cb3.fill()
+      }
+      cb2.circle(260, 500, 100)
+      cb2.fill()
+    }
+    cb1.circle(260, 500, 50)
+    cb1.fill()
+  }
+}

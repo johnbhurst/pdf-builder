09263716980a1cdd75265ddb6bc6d8b1971f9338
John Hurst <john.b.hurst@gmail.com>
2010-09-21 18:19:56 +1200

    Got template.withText() working using Category directly -- not ideal.

--- examples/classroom/intro/HelloWorld08.groovy
+++ examples/classroom/intro/HelloWorld08.groovy
@@ -2,6 +2,7 @@
 import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
 import com.lowagie.text.pdf.BaseFont
 import com.lowagie.text.pdf.PdfTemplate
+import nz.co.skepticalhumorist.pdfbuilder.PdfCategory
 
 def RESULT = "build/examples/classroom/intro/hello08.pdf"
 BaseFont bf = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
@@ -16,10 +17,12 @@ new PDFBuilder(new FileOutputStream(RESULT)).document() {
     cb.showText("llo")
   }
   PdfTemplate tmp = writer.directContent.createTemplate(250, 25)
-  tmp.withText {cb ->
-    cb.setFontAndSize(bf, 12)
-    cb.moveText(0, 7)
-    cb.showText("He")
+  use (PdfCategory) {
+    tmp.withText {cb ->
+      cb.setFontAndSize(bf, 12)
+      cb.moveText(0, 7)
+      cb.showText("He")
+    }
   }
   writer.directContent.addTemplate(tmp, 36, 781)
 }

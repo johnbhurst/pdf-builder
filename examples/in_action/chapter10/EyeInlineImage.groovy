ead3a40415ea72c0731e7338f77f04a09ad8e604
John Hurst <john.b.hurst@gmail.com>
2010-09-23 07:19:42 +1200

    Added EyeInlineImage.groovy example from iText in Action book.

---
+++ examples/in_action/chapter10/EyeInlineImage.groovy
@@ -0,0 +1,16 @@
+@Grab("nz.co.skepticalhumorist:pdf-builder:latest.release")
+import nz.co.skepticalhumorist.pdfbuilder.PDFBuilder
+import com.lowagie.text.Image
+
+String resourcePath(String path) {
+  System.properties["itext.examples.home"] + "/resources/in_action/chapter10/" + path
+}
+
+new PDFBuilder(new FileOutputStream("build/examples/in_action/chapter10/EyeInlineImage.pdf")).document() {
+  Image eye = Image.getInstance(resourcePath("iTextLogo.gif"))
+  eye.setAbsolutePosition(36, 780)
+  writer.directContent.with {
+    addImage(eye, true)
+    addImage(eye, 271, -50, -30, 550, 100, 100, true)
+  }
+}

0c1b450ddb18da260a8eb9ec89e084e5c401a78a
John Hurst <john.b.hurst@gmail.com>
2010-09-23 20:10:32 +1200

    Improved clarity of example by passing named vals to closures instead of using list indexes.

--- examples/in_action/chapter11/DeviceColor.groovy
+++ examples/in_action/chapter11/DeviceColor.groovy
@@ -16,9 +16,9 @@ new PDFBuilder(new FileOutputStream("build/examples/in_action/chapter11/DeviceCo
       [191   , 360, 770, 36, 36],
       [223   , 416, 770, 36, 36],
       [255   , 470, 770, 36, 36]
-    ].each {
-      setColorFill(new GrayColor(it[0]))
-      rectangle(it[1], it[2], it[3], it[4])
+    ].each {c, x, y, w, h ->
+      setColorFill(new GrayColor(c))
+      rectangle(x, y, w, h)
       fillStroke()
     }
     [
@@ -31,9 +31,9 @@ new PDFBuilder(new FileOutputStream("build/examples/in_action/chapter11/DeviceCo
       [0.75f , 360, 716, 36, 36],
       [0.875f, 416, 716, 36, 36],
       [1     , 470, 716, 36, 36]
-    ].each {
-      setGrayFill(it[0])
-      rectangle(it[1], it[2], it[3], it[4])
+    ].each {c, x, y, w, h ->
+      setGrayFill(c)
+      rectangle(x, y, w, h)
       fillStroke()
     }
     [
@@ -45,9 +45,9 @@ new PDFBuilder(new FileOutputStream("build/examples/in_action/chapter11/DeviceCo
       [1f  , 0   , 1f  , 306, 662, 36, 36],
       [1f  , 1f  , 0   , 360, 662, 36, 36],
       [1f  , 1f  , 1f  , 416, 662, 36, 36]
-    ].each {
-      setColorFill(new Color(it[0], it[1], it[2]))
-      rectangle(it[3], it[4], it[5], it[6])
+    ].each {r, g, b, x, y, w, h ->
+      setColorFill(new Color(r, g, b))
+      rectangle(x, y, w, h)
       fillStroke()
     }
     [
@@ -55,9 +55,9 @@ new PDFBuilder(new FileOutputStream("build/examples/in_action/chapter11/DeviceCo
       [0x00, 0x00, 0xFF,  90, 608, 36, 36],
       [0x00, 0xFF, 0x00, 144, 608, 36, 36],
       [0x00, 0xFF, 0xFF, 198, 608, 36, 36]
-    ].each {
-      setRGBColorFill(it[0], it[1], it[2])
-      rectangle(it[3], it[4], it[5], it[6])
+    ].each {r, g, b, x, y, w, h ->
+      setRGBColorFill(r, g, b)
+      rectangle(x, y, w, h)
       fillStroke()
     }
     [
@@ -65,9 +65,9 @@ new PDFBuilder(new FileOutputStream("build/examples/in_action/chapter11/DeviceCo
       [1f, 0f, 1f, 306, 608, 36, 36],
       [1f, 1f, 0f, 360, 608, 36, 36],
       [1f, 1f, 1f, 416, 608, 36, 36]
-    ].each {
-      setRGBColorFillF(it[0], it[1], it[2])
-      rectangle(it[3], it[4], it[5], it[6])
+    ].each {r, g, b, x, y, w, h ->
+      setRGBColorFillF(r, g, b)
+      rectangle(x, y, w, h)
       fillStroke()
     }
     [
@@ -79,9 +79,9 @@ new PDFBuilder(new FileOutputStream("build/examples/in_action/chapter11/DeviceCo
       [1f  , 0f  , 0f  , 0f  , 306, 554, 36, 36],
       [1f  , 0f  , 0f  , 0.5f, 360, 554, 36, 36],
       [0f  , 0f  , 0f  , 1f  , 416, 554, 36, 36]
-    ].each {
-      setColorFill(new CMYKColor(it[0], it[1], it[2], it[3]))
-      rectangle(it[4], it[5], it[6], it[7])
+    ].each {cya, mag, yel, black, x, y, w, h ->
+      setColorFill(new CMYKColor(cya, mag, yel, black))
+      rectangle(x, y, w, h)
       fillStroke()
     }
     [
@@ -89,9 +89,9 @@ new PDFBuilder(new FileOutputStream("build/examples/in_action/chapter11/DeviceCo
       [0x00, 0xFF, 0xFF, 0x00,  90, 500, 36, 36],
       [0x00, 0xFF, 0xFF, 0x0F, 144, 500, 36, 36],
       [0xFF, 0xFF, 0x00, 0x00, 198, 500, 36, 36]
-    ].each {
-      setCMYKColorFill(it[0], it[1], it[2], it[3])
-      rectangle(it[4], it[5], it[6], it[7])
+    ].each {cya, mag, yel, black, x, y, w, h ->
+      setCMYKColorFill(cya, mag, yel, black)
+      rectangle(x, y, w, h)
       fillStroke()
     }
     [
@@ -99,9 +99,9 @@ new PDFBuilder(new FileOutputStream("build/examples/in_action/chapter11/DeviceCo
       [1f, 0f, 1f, 0f  , 306, 500, 36, 36],
       [1f, 0f, 1f, 0.5f, 360, 500, 36, 36],
       [0f, 0f, 0f, 1f  , 416, 500, 36, 36]
-    ].each {
-      setCMYKColorFillF(it[0], it[1], it[2], it[3])
-      rectangle(it[4], it[5], it[6], it[7])
+    ].each {cya, mag, yel, black, x, y, w, h ->
+      setCMYKColorFillF(cya, mag, yel, black)
+      rectangle(x, y, w, h)
       fillStroke()
     }
   }
